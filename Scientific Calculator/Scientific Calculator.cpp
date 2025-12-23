d6d37a645aca13b7a7ba9dd5d5baba47262c4949
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-12-23 20:20:03 +0530

    Add project files.

---
+++ Scientific Calculator/Scientific Calculator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	int choice;
+	double num1, num2, result;
+	do {
+		cout << "\n=== SCIENCETIFIC CALCULATOR ===\n";
+		cout << "1. Addtion(+)\n";
+		cout << "2. Substraction(-)\n";
+		cout << "3. Multiplication(*)\n";
+		cout << "4. Division(/)\n";
+		cout << "5. Power(x^y)\n";
+		cout << "6. Square Roots(x^2)\n";
+		cout << "7. Sine(sin x in degrees)\n";
+		cout << "8. Cosine(cos x in degrees)\n";
+		cout << "9. Tangent(tan x in degrees)\n";
+		cout << "10. Logaritime(ln x)\n";
+		cout << "11. Exit\n";
+		cout << "Enter your choice=";
+		cin >> choice;
+
+
+	}
+}
